9377d58212b79a8bab4094111f3e132e4bd9e268
sunyongjian <704928775@qq.com>
2016-06-18 01:10:20 +0800

    1test

---
+++ js/utilsok.js
@@ -0,0 +1,274 @@
+var utils = (function () {
+    var flag = "getComputedStyle" in window;
+    function listToArray(likeArray) {
+        if (flag) {
+            return Array.prototype.slice.call(likeArray, 0);
+        }
+        var ary = [];
+        for (var i = 0; i < likeArray.length; i++) {
+            ary[ary.length] = likeArray[i];
+        }
+        return ary;
+    }
+    function jsonParse(text) {
+        return "JSON" in window ? JSON.parse(text) : eval("(" + text + ")");
+
+    }
+    function offset(ele) {
+        var left = ele.offsetLeft, top = ele.offsetTop, parent = ele.offsetParent;
+        while (parent) {
+            if (window.navigator.userAgent.indexOf('MSIE 8.0') !== -1) {
+                left += parent.clientLeft;
+                top += parent.clientTop;
+            }
+            left += parent.offsetLeft;
+            top += parent.offsetTop;
+            parent = parent.offsetParent;
+        }
+        return {left: left, top: top}
+    }
+    function getWin(attr, val) {//一个参数 是读取，两个参数还可以赋值~~
+        if (typeof val !== "undefined") {
+            document.documentElement[attr] = val;
+            document.body[attr] = val;
+        }
+        return document.documentElement[attr] || document.body[attr];
+    }
+    function children(curEle, tagName) {
+        var ary = [];
+        if (!flag) {
+            var nodeList = curEle.childNodes;
+            for (var i = 0, len = nodeList.length; i < len; i++) {
+                var curNode = nodeList[i];
+                curNode.nodeType === 1 ? ary[ary.length] = curNode : null;
+            }
+            nodeList = null;
+        } else {
+            ary = Array.prototype.slice.call(curEle.children)
+        }
+        if (typeof tagName === "string") {
+            for (var k = 0; k < ary.length; k++) {
+                var curEleNode = ary[k];
+                if (curEleNode.nodeName.toLowerCase() !== tagName.toLowerCase()) {
+                    ary.splice(k, 1);
+                    k--;
+                }
+            }
+        }
+        return ary;
+    }
+    function prev(curEle){
+        if(flag){
+            return curEle.previousElementSibling;
+        }
+        var pre=curEle.previousSibling;
+        while (pre&&pre.nodeType!==1){
+            pre=pre.previousSibling;
+        }
+        return pre;
+    }
+    function next(curEle){
+        if(flag){
+            return curEle.nextElementSibling;
+        }
+        var nex=curEle.nextSibling;
+        while (nex&&nex.nodeType!==1){
+            nex=nex.nextSibling;
+        }
+        return nex;
+    }
+    function prevAll(curEle){
+        var ary=[];
+        var pre=this.prev(curEle);
+        while (pre){
+            ary.unshift(pre);
+            pre=this.prev(pre);
+        }
+        return ary;
+    }
+    function nextAll(curEle){
+        var ary=[];
+        var nex=this.next(curEle);
+        while (nex){
+            ary.push(nex);
+            nex=this.next(nex);
+        }
+        return ary;
+    }
+    function sibling(curEle){
+        var pre=this.prev(curEle);
+        var nex=this.next(curEle);
+        var ary=[];
+        pre?ary.push(pre):null;
+        nex?ary.push(nex):null;
+        return ary;
+    }
+    function siblings(curEle){
+        return this.prevAll(curEle).concat(this.nextAll(curEle));
+    }
+    function index(curEle){
+        return this.prevAll(curEle).length;
+    }
+    function firstChild(curEle){
+        var chs=this.children(curEle);
+        return chs.length>0?chs[0]:null;
+    }
+    function lastChild(curEle){
+        var chs=this.children(curEle);
+        return chs.length>0?chs[chs.length-1]:null;
+    }
+    function append(newEle,container){
+        container.appendChild(newEle);
+    }
+    function prepend(newEle,container){
+        var fir=this.firstChild(container);
+        if(fir){
+            container.insertBefore(newEle,fir);
+            return;
+        }
+        container.appendChild(newEle);
+    }
+    function insertBefore(newEle,oldEle){
+        oldEle.parentNode.insertBefore(newEle,oldEle)
+    }
+    function insertAfter(newEle,oldEle){
+        var nex=this.next(oldEle);
+        if(nex){
+            oldEle.parentNode.insertBefore(newEle,nex)
+        }
+        oldEle.parentNode.appendChild(newEle);
+    }
+    function hasClass(curEle,className){
+        var reg=new RegExp("(^| +)"+className+"( +|$)");
+        return reg.test(curEle.className);
+    }
+    function addClass(curEle,className){
+        var ary=className.replace(/(^ +| +$)/g,""). split(/ +/g);
+        for(var i= 0,len=ary.length;i<len;i++){
+            var curName=ary[i];
+            if(!hasClass(curEle,curName)){
+                curEle.className+=" "+curName;
+            }
+        }
+    }
+    function removeClass(curEle,className){
+        var ary=className.replace(/(^ +| +$)/g,"").split(/ +/g);
+        for(var i= 0,len=ary.length;i<len;i++){
+            var curName=ary[i];
+            if(hasClass(curEle,curName)){
+                var reg=new RegExp("(^| +)"+curName+"( +|$)","g");
+                curEle.className=curEle.className.replace(reg," ")
+            }
+        }
+    }
+
+    function getElementsByClass(strClass,context){
+        context=context||document;
+        if(flag){
+            return this.listToArray(context.getElementsByClassName(strClass));
+        }
+        var ary=[];
+        var strClassAry=strClass.replace(/(^ +| +$)/g,"").split(/ +/g);
+        var nodeList=context.getElementsByTagName("*");
+        for(var i=0,len=nodeList.length;i<len;i++){
+            var curNode=nodeList[i];
+            var isOk=true;
+            for(var k=0;k<strClassAry.length;k++){
+                var reg=new RegExp("(^| +)"+strClassAry[k]+"( +|$)");
+                if(!reg.test(curNode.className)){
+                    isOk=false;
+                    break;
+                }
+            }
+            if(isOk){
+                ary[ary.length]=curNode;
+            }
+        }
+        return ary;
+    }
+
+    function getCss(attr) {
+        var val = null, reg = null;
+        if (flag) {
+            val = window.getComputedStyle(this, null)[attr];
+        } else {
+            if (attr === "opacity") {
+                val = this.currentStyle["filter"];
+                reg = /^alpha\(opacity=((?:\d|(?:[1-9]\d+))(?:\.\d+)?)\)$/;
+                var tempVal = reg.exec(val);
+                val = tempVal ? tempVal[1] / 100 : 1;
+            } else {
+                val = this.currentStyle[attr];
+            }
+        }
+        reg = /^[+-]?(\d|([1-9]\d+))(\.\d+)?(px|pt|em|rem)?$/;
+        return reg.test(val) ? parseFloat(val) : val;
+    }
+    function setCss(attr,value){
+        if(attr==="float"){
+            this["style"]["cssFloat"]=value;
+            this["style"]["styleFloat"]=value;
+            return;
+        }
+        if(attr==="opacity"){
+            this["style"]["opacity"]=value;
+            this["style"]["filter"]="alpha(opacity="+value/100+")";
+            return;
+        }
+        var reg=/^(width|height|top|bottom|left|right|((margin|padding)(Top|Bottom|Left|Right)?)$)/;
+        if(reg.test(attr)){
+            if(!isNaN(value)){
+                value+="px";
+            }
+        }
+        this["style"][attr]=value;
+    }
+    function setGroupCss(options) {
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                setCss.call(this,key,options[key]);
+            }
+        }
+    }
+    function css(curEle){
+        var ary=Array.prototype.slice.call(arguments,1);
+        var argTwo=arguments[1];
+        if(typeof argTwo==="string"){
+            if(typeof arguments[2]==="undefined"){
+                return getCss.apply(curEle,ary);
+            }
+            setCss.apply(curEle,ary);
+        }
+        argTwo=argTwo||0;
+        if(argTwo.toString()==="[object Object]"){
+            setGroupCss.apply(curEle,ary);
+        }
+    }
+
+
+    return {
+        listToArray: listToArray,
+        jsonParse:jsonParse ,
+        offset: offset,
+        getWin: getWin,
+        children: children,
+        prev:prev,
+        next:next,
+        prevAll:prevAll,
+        nextAll:nextAll,
+        sibling:sibling,
+        siblings:siblings,
+        index:index,
+        firstChild:firstChild,
+        lastChild:lastChild,
+        append:append,
+        prepend:prepend,
+        insertBefore:insertBefore,
+        insertAfter:insertAfter,
+        hasClass:hasClass,
+        addClass:addClass,
+        removeClass:removeClass,
+        getElementsByClass:getElementsByClass,
+        css:css
+    }
+})()
